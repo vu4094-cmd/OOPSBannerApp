8af152b52ee2335e0612813f71394c27624e974f
vu4094-cmd <vu4094@srmist.edu.in>
2026-03-10 09:22:31 +0530

    Update uc2.java

---
+++ uc2.java
@@ -0,0 +1,18 @@
+/**
+ * Project: OOPS Banner App
+ * Use Case: UC2
+ * Goal: Display "OOPS" as a large banner using '*' characters
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****     ***** ");
+        System.out.println("*     *  *     *  *     *   *     ");
+        System.out.println("*     *  *     *  *****     ***** ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println(" *****    *****   *        ***** ");
+
+    }
+}
